8fcf17ec6025bfff373e2a2ef6e8a39b1a07bf4e
Tomer <tomermi123@gmail.com>
2024-04-02 14:23:40 +0300

    fixing from reviews in PR Tomer1 #1

--- js/details.js
+++ js/details.js
@@ -2,14 +2,14 @@
 document.addEventListener('DOMContentLoaded', async () => {
     const searchParams = new URLSearchParams(window.location.search);
     const countryName = searchParams.get('country');
-
     const loader = document.querySelector('.loader');
+    const COUNTRY_BY_NAME_URL = `https://restcountries.com/v3.1/name/${countryName}`;
 
     if (countryName) {
         loader.style.display = 'none';
-        const fetchCountryDetails = async (countryName) => {
+        const fetchCountryDetails = async () => {
             try {
-                const res = await axios.get(`https://restcountries.com/v3.1/name/${countryName}`);
+                const res = await axios.get(COUNTRY_BY_NAME_URL);
                 const countryDetails = res.data[0];
                 renderCountryDetails(countryDetails);
             } catch (err) {
@@ -20,24 +20,15 @@ document.addEventListener('DOMContentLoaded', async () => {
         const renderCountryDetails = (country) => {
             const countryDetailsContainer = document.querySelector('.country-details');
 
-            // Check if the properties exist in the country object
-            const countryName = country.name ? country.name.common : 'N/A';
-            const flagSrc = country.flags ? country.flags.png : '';
-            const population = country.population ? numberWithCommas(country.population) : 'N/A';
-            const continent = country.continents ? country.continents[0] : 'N/A';
-            const capital = country.capital[0] || 'N/A';
-            const languages = country.languages ? Object.values(country.languages).join(', ') : 'N/A';
-            const subregion = country.subregion || 'N/A';
-            const currencies = country.currencies
-                ? `${Object.values(country.currencies)[0].name} (${Object.values(country.currencies)[0].symbol})`
-                : 'N/A';
+            if (country){
+                const {name, flagSrc, population,
+                    continent, capital, languages,
+                    subregion, currencies } = getCountryDetails(country);
 
-
-            // Construct HTML to display country details
             countryDetailsContainer.innerHTML = `
-                <p><strong>Country Name</strong> ${countryName}
+                <p><strong>Country Name</strong> ${name}
                     <br>
-                    <img src="${flagSrc}" alt="${countryName} Flag" class="flag-image"/>
+                    <img src="${flagSrc}" alt="${name} Flag" class="flag-image"/>
                     <p><strong>Population:</strong> ${population}</p>
                     <p><strong>Continent:</strong> ${continent}</p>
                     <p><strong>Capital:</strong> ${capital}</p>
@@ -46,18 +37,39 @@ document.addEventListener('DOMContentLoaded', async () => {
                     <p><strong>Currencies:</strong> ${currencies}</p>
                 </p>                
             `;
+            } else {
+                countryDetailsContainer.innerHTML = 'Country details not found';
+            }
         };
 
+        const getCountryDetails = (country) => {
+            const numberWithCommas = (number) => number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+            const NA = 'N/A';
+
+            const name = country.name ? country.name.common : NA;
+            const flagSrc = country.flags ? country.flags.png : '';
+            const population = country.population ? numberWithCommas(country.population) : NA;
+            const continent = country.continents ? Object.values(country.continents).join(', ') : NA;
+            const capital = country.capital ? country.capital[0] : NA;
+            const languages = country.languages ? Object.values(country.languages).join(', ') : NA;
+            const subregion = country.subregion || NA;
+            const currencies = country.currencies
+                ? `${Object.values(country.currencies)[0].name} (${Object.values(country.currencies)[0].symbol})`
+                : NA;
+
+            return {
+                name, flagSrc, population,
+                continent, capital, languages,
+                subregion, currencies
+            };
+        }
+
         // Fetch country details on page load
-        await fetchCountryDetails(countryName);
+        await fetchCountryDetails();
     } else {
         console.log('Country name is not provided.');
     }
 });
-
-const numberWithCommas = (number) => {
-    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
 
 const themeToggle = document.querySelector('.theme-toggle');
 const body = document.body;
